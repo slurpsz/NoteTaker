18536e309465de499acec7fc93af8e20194237b2
slurpeesz <khtbee@gmail.com>
2023-03-24 18:29:44 -0700

    add saveData file, edited htmlRouter

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,18 @@
 const router = require('express').Router();
 const saveData = require('../db/saveData');
+
+router.get('/', (req, res) => {
+  saveData
+  .getNotes()
+  .then(notes => res.json(notes))
+  .catch(err => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+  saveData
+  .addNote(req.body)
+  .then((note) => res.json(note))
+  .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router; 
